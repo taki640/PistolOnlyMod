ff99161285e9746fbc2f2c9584a1b3deadf8b519
Taki Takanashi <gustavo.almeidafn@gmail.com>
2023-11-12 00:39:05 -0300

    Fixed softlock at prologue

--- PistolOnlyMod/Source/Script.cpp
+++ PistolOnlyMod/Source/Script.cpp
@@ -1,9 +1,9 @@
 #include "Script.h"
 
-#include "Keyboard.h"
 #include "ScriptHookV/main.h"
 #include "ScriptHookV/natives.h"
 #include "ScriptHookV/enums.h"
+#include "Keyboard.h"
 
 namespace PistolOnly
 {
@@ -30,6 +30,19 @@ namespace PistolOnly
 		}
 	}
 
+	bool IsPlayingPrologue(const Ped& player)
+	{
+		const Vector3 pos = ENTITY::GET_OFFSET_FROM_ENTITY_IN_WORLD_COORDS(player, 0.0f, 0.0f, 0.0f);
+		// i hate this, would be better to check the mission name instead of location.
+		// i found a stat called MISSION_NAME but couldn't get the value of it.
+		return strcmp(ZONE::GET_NAME_OF_ZONE(pos.x, pos.y, pos.z), "PROL") == 0;
+	}
+
+	bool IsPlayerTrevor(const Ped& player)
+	{
+		return PED::IS_PED_MODEL(player, MISC::GET_HASH_KEY("PLAYER_TWO"));	// PLAYER_TWO = trevor model
+	}
+
 	void CheckAndReplaceWeapons()
 	{
 		const Ped player = PLAYER::PLAYER_PED_ID();
@@ -38,7 +51,22 @@ namespace PistolOnly
 
 		if (currentWeapon != eWeapon::WeaponUnarmed && currentWeapon != eWeapon::WeaponPistol)
 		{
-			const int pistolAmmoCount = WEAPON::GET_AMMO_IN_PED_WEAPON(player, eWeapon::WeaponPistol);
+			int pistolAmmoCount = WEAPON::GET_AMMO_IN_PED_WEAPON(player, eWeapon::WeaponPistol);
+
+			static bool fixedPrologueSoftlock = false;
+			if (!fixedPrologueSoftlock && IsPlayingPrologue(player) && IsPlayerTrevor(player))
+			{
+				// during prologue the player has no pistol ammo and the
+				// player cannot proceed the scene where trevor saves michael
+				if (pistolAmmoCount == 0)
+				{
+					pistolAmmoCount = 1;
+					// this is ok to be set to true here because the first time
+					// the player controls trevor is where the softlock happens
+					fixedPrologueSoftlock = true;
+				}
+			}
+
 			WEAPON::REMOVE_ALL_PED_WEAPONS(player, FALSE);
 			WEAPON::GIVE_WEAPON_TO_PED(player, eWeapon::WeaponPistol, pistolAmmoCount, FALSE, TRUE);
 		}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Pistol Only Mod
-A Grand Theft Auto V mod that restricts the player to only equip a pistol.
+A Grand Theft Auto V mod that restricts the player weapon to a pistol.
